11f8143643f91cb85cc9c59f9f8dc959cbc84c40
Hiep Nguyen <thienhiep171193@gmail.com>
2022-08-17 22:05:54 +0700

    Update config for ts store

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
     defaultLanguageCode: 'EN',
     storeDomain:
       // @ts-ignore
-      Oxygen?.env?.SHOPIFY_STORE_DOMAIN || 'hiep-nguyen-hydrogen.myshopify.com',
+      Oxygen?.env?.SHOPIFY_STORE_DOMAIN || 'hiep-nguyen-hydrogen-js.myshopify.com',
     storefrontToken:
       // @ts-ignore
       Oxygen?.env?.SHOPIFY_STOREFRONT_API_PUBLIC_TOKEN ||
-      'd8ae78349cf7810768360a57fbe08577',
+      '9474f5f0ea1f480f4d2d1cd8d85e77d8',
     storefrontApiVersion: '2022-07',
   }),
   session: CookieSessionStorage('__session', {
